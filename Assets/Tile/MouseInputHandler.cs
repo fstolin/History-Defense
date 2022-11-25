dc5bee17266a0f60d94c48d5b29bd0bf44e8df4c
gogomaester <stolin.f@gmail.com>
2022-11-25 18:38:48 +0100

    Adding mouseInput

---
+++ Assets/Tile/MouseInputHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputHandler : MonoBehaviour
+{
+    [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject tower;
+
+    private void OnMouseDown()
+    {
+        if (isPlaceable)
+        {
+            PlaceTower();
+            isPlaceable = false;
+        }
+    }
+
+    private void PlaceTower()
+    {
+        // Place tower
+        Instantiate(tower, this.transform.position, Quaternion.identity);
+    }
+}
